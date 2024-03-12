c0c351e93a749406cb598f019388eeb9f0c67ef2
Amardeep Singh <amardeepsinghbrar@gmail.com>
2024-03-12 19:36:48 +0530

    codefix

--- Iaac/provider.tf
+++ Iaac/provider.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         bucket                      = "mys3bucket-terraform"
         key                         = "prod/terraform.tfstate"
-        dynamodynamodb_table        = "terraform-lock"
+        dynamodb_table              = "terraform-lock"
         
         }
 
