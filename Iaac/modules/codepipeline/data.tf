3fdfcca66632bff6823decea7836231e10e3853a
Amardeep Singh <amardeepsinghbrar@gmail.com>
2024-03-12 19:38:58 +0530

    codefix

--- Iaac/modules/codepipeline/data.tf
+++ Iaac/modules/codepipeline/data.tf
@@ -1,11 +1,11 @@
-data "terraform_remote_state" "remote_network"{
-    backend = "s3"
-    config = {
-      bucket    = var.s3_terraform_state_store_bucket
-      key       = "${var.environment}/terraform.tfstate"
-      region    = var.region
-      profile   = var.profile
-    }
-}
+# data "terraform_remote_state" "remote_network"{
+#     backend = "s3"
+#     config = {
+#       bucket    = var.s3_terraform_state_store_bucket
+#       key       = "${var.environment}/terraform.tfstate"
+#       region    = var.region
+#       profile   = var.profile
+#     }
+# }
 
 data "aws_caller_identity" "current" {}
