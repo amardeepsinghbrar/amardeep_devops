3234e831d7e666e0e991955ec0ef0a06a3171bac
Amardeep Singh <amardeepsinghbrar@gmail.com>
2024-03-12 19:32:32 +0530

    codefix

--- Iaac/modules/codepipeline/data.tf
+++ Iaac/modules/codepipeline/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "remote_network"{
     backend = "s3"
     config = {
       bucket    = var.s3_terraform_state_store_bucket
-      key       = "prod/ecr/terraform.tfstate"
+      key       = "${var.environment}/terraform.tfstate"
       region    = var.region
       profile   = var.profile
     }
