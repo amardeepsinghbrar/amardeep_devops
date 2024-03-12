aa8b05aebce127c8655b77459a9a1eae4bec8fef
Amardeep Singh <amardeepsinghbrar@gmail.com>
2024-03-12 19:37:46 +0530

    codefix

--- Iaac/provider.tf
+++ Iaac/provider.tf
@@ -3,7 +3,7 @@ terraform {
         bucket                      = "mys3bucket-terraform"
         key                         = "prod/terraform.tfstate"
         dynamodb_table              = "terraform-lock"
-        
+        region = "us-east-1"   
         }
 
     required_providers {
@@ -15,6 +15,5 @@ terraform {
   }
 
 provider "aws" {
-  region = "us-east-1"
   profile = "default"
 }
